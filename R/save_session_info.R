70a5e101ab4970c09b2437ed17bbdc7e04866428
Ben Davies <bldavies@users.noreply.github.com>
2020-04-05 15:05:26 +1200

    Add save_session_info function

---
+++ R/save_session_info.R
@@ -0,0 +1,12 @@
+#' Save session information
+#'
+#' \code{save_session_info} writes the output of
+#' \code{sessioninfo::session_info()} to file.
+#'
+#' @param path Path of written file.
+#'
+#' @export
+save_session_info <- function(path = "session.log") {
+  options(width = 80)
+  writeLines(capture.output(sessioninfo::session_info()), path)
+}
