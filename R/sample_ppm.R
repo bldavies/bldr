fd66a50e7f54ddf5395190ac93443fa5739ca280
Ben Davies <bldavies@users.noreply.github.com>
2020-04-05 22:01:45 +1200

    Add sample_ppm function

---
+++ R/sample_ppm.R
@@ -0,0 +1,22 @@
+#' Sample planted partition model
+#'
+#' \code{sample_ppm} generates a random graph using the planted partition model,
+#'   which is a special case of the stochastic block model.
+#'
+#' @param memb Vector of community memberships.
+#' @param p Probability that vertices in same community are adjacent.
+#' @param q Probability that vertices in different communities are adjacent.
+#' @param seed Initial seed for edge randomisation (optional).
+#'
+#' @return An igraph graph.
+#'
+#' @export
+sample_ppm <- function(memb, p, q, seed = NULL) {
+  mat <- t(combn(seq_along(memb), 2))
+  prob <- c(q, p)[1 + (memb[mat[, 1]] == memb[mat[, 2]])]
+  if (!is.null(seed)) {
+    set.seed(seed)
+  }
+  el <- mat[which(runif(nrow(mat)) < prob), ]
+  igraph::graph_from_edgelist(el, directed = FALSE)
+}
